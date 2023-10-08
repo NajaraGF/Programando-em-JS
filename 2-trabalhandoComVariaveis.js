a073d6cbf6be347a8bd502716a21fc92b02d7015
Najha <najhateste@gmail.com>
2023-10-08 16:22:10 -0300

    Segunda aula js

---
+++ 2-trabalhandoComVariaveis.js
@@ -0,0 +1,20 @@
+console.log("Trabalhando com variáveis");
+//JS é case sensitive
+
+const idade = 29;
+
+
+
+console.log(idade);
+//boa prática, escreve com letra minúscula e coloque sempre a delclaração da variável, exemplo: "const idade = 29;"
+
+
+const nome = "Lais";
+const Sobrenome = "Silva";
+const Idade = 30;
+const anoDeNascimento = 1993;
+
+//não fazer
+//mes = "Janiero";
+
+
