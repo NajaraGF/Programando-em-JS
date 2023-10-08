f6e54702e40ef4ec1ac7a0dcccd431005ee9e7ac
Najha <najhateste@gmail.com>
2023-10-08 16:35:12 -0300

    Quarta aula em js

---
+++ 4-conversaoDeTipos.js
@@ -0,0 +1,13 @@
+console.log("Conversão de Tipos");
+
+    
+console.log("ano" + 2020);
+console.log("2" + "2");
+console.log(parseInt("2") + parseInt("2"));
+
+console.log("3"/"2");
+console.log("Ricardo"/"2"); //Not a Number - Nan onde tem o nome e número, então conseguerira converter
+
+console.log(6.5);
+console.log(6,5);
+
